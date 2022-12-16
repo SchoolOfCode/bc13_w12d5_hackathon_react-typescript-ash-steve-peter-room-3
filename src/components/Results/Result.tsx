93413869b6f4485f13d6f244a2480394c7c40f88
PeterM-13 <petershotbox@gmail.com>
2022-12-16 12:08:15 +0000

    added some css

--- src/components/Results/Result.tsx
+++ src/components/Results/Result.tsx
@@ -11,7 +11,7 @@ const Result = (props:ResultProps) => {
         <div className="Results">
             <img src={`http://openweathermap.org/img/wn/${data.list[0].weather.icon}@2x.png`} alt="current weather"/>
             <p>{data.list[0].weather.description}</p>
-            <WeatherTiles image={"../images/temperature"} title="Temperature" data={data.list[0].main.temp}/>
+            <WeatherTiles image={"../images/temperature"} title="Temperature" data={data.list[0].main.temp-273}/>
             <WeatherTiles image={"../images/wind"} title="Wind" data={data.list[0].wind.speed}/>
             <WeatherTiles image={"../images/humidity"} title="Humidity" data={data.list[0].main.humidity}/>
             <WeatherTiles image={"../images/pressure"} title="Pressure" data={data.list[0].main.pressure}/>
