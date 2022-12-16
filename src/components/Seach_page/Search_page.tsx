a687b9de1aa874d5bb31fa167b2aac04952ae9f0
Steve Beecheno <iamhacktinium@gmail.com>
2022-12-16 15:02:51 +0000

    Form added. Fetch now uses dynamic value

--- src/components/Seach_page/Search_page.tsx
+++ src/components/Seach_page/Search_page.tsx
@@ -3,18 +3,43 @@ import Result from "../Results/Result";
 
 export default function SearchPage() {
 	const [data, setData] = useState(null);
+	const [inputText, setInputText] = useState('');
+	const [location, setLocation] = useState('London');
+
 
 	useEffect(() => {
 		async function getWeather() {
-			const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=b11b706c0bdb85fd3b45ecc8262901f9
+			
+				const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b11b706c0bdb85fd3b45ecc8262901f9
+
 			`);
 			const data = await response.json();
-			setData(data);
+			console.log(data.cod)
+				if (data.cod !== 200) {
+					return;
+				}
+				setData(data);
+			
+			
 		}
 		getWeather();
-	}, []);
+	}, [location]);
 
 
+	const handleClick = (e: any) => {
+		e.preventDefault();
+		setLocation(inputText);
+	}
 
-	return (data && <Result data={data} />)
+
+	return (
+		<div>
+			<form action="submit">
+				<input type="text" value={inputText}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setInputText(e.target.value)}} />
+				<button onClick={handleClick}>Search</button>
+			</form>
+			{data && <Result data={data} />}
+		</div>
+	)
 }
