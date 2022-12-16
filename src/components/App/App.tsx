7726824cae636c593c2d0f543d298c18793b52ff
Steve Beecheno <iamhacktinium@gmail.com>
2022-12-16 12:15:06 +0000

    Typo fixed. Fetch completed.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import Header from '../Header/Header';
+import Header from '../header/Header';
 import './App.css';
 
 
--- src/components/Seach_page/Search_page.tsx
+++ src/components/Seach_page/Search_page.tsx
@@ -1,21 +1,25 @@
-import { useEffect, useState } from 'react';
-
-export default function Search_page(props: any) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(`api.openweathermap.org/data/2.5/forecast?q=${props.city}}&appid=7ca83e00916d7d886d1132beb93560f4`);
-      const json = await response.json();
-      setData(json);
-    }
-    fetchData();
-  }, [props.city]);
+import { useEffect, useState } from "react";
 
 
-  if (data) {
-    return <div>{JSON.stringify(data)}</div>;
-  } else {
-    return <div>Loading...</div>;
-  }
+export default function SearchPage() {
+	const [data, setData] = useState(null);
+
+	useEffect(()=>{
+		async function getWeather(){
+			const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=b11b706c0bdb85fd3b45ecc8262901f9
+			`);
+			const data = await response.json();
+			console.log(data);
+			setData(data);
+		}
+		getWeather()
+	}, [])
+	
+
+	if (data) {
+		return <div>{JSON.stringify(data)}</div>;
+	} else {
+		return <div>Loading...</div>;
+	}
 }
+
